87c031e87c888e819b934bc342415d30723864e0
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-05-20 11:03:01 +0530

    access with parameter

--- src/main/java/com/example/demo/PageController.java
+++ src/main/java/com/example/demo/PageController.java
@@ -37,91 +37,15 @@ public class PageController {
     public String login() {
         return "login";
     }
-    
-    @GetMapping("/welcome")
-    public String getWelcome() {
-        return "welcome";
-    }
 
-//    @PostMapping("/login")
-//    public String login(@RequestParam String username, @RequestParam String password, Model model) {
-//        try {
-//            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-//            SecurityContextHolder.getContext().setAuthentication(authentication);
-//            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-//            String token = "Bearer " + jwtTokenProvider.generateToken(userDetails);
-//            model.addAttribute("token", token);
-//            model.addAttribute("username", userDetails.getUsername());
-//            model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
-//            return "home";
-//        } catch (Exception e) {
-//            model.addAttribute("error", "Invalid username or password");
-//            return "login";
-//        }
-//    }
-//
-//    @GetMapping("/home")
-//    public String home(@RequestParam String token, Model model) {
-//        if (token != null && token.startsWith("Bearer ")) {
-//            token = token.substring(7);
-//            String username = jwtTokenProvider.extractUsername(token);
-//            UserDetails userDetails = employeeDetailsService.loadUserByUsername(username);
-//            if (jwtTokenProvider.validateToken(token, userDetails)) {
-//                model.addAttribute("token", "Bearer " + token);
-//                model.addAttribute("username", userDetails.getUsername());
-//                model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
-//                return "home";
-//            }
-//        }
-//        return "redirect:/login";
-//    }
-//
-//    @GetMapping("/admin/dashboard")
-//    public String adminDashboard(@RequestParam String token, Model model) {
-//        if (token != null && token.startsWith("Bearer ")) {
-//            token = token.substring(7);
-//            String username = jwtTokenProvider.extractUsername(token);
-//            UserDetails userDetails = employeeDetailsService.loadUserByUsername(username);
-//            if (jwtTokenProvider.validateToken(token, userDetails)) {
-//                model.addAttribute("token", "Bearer " + token);
-//                model.addAttribute("username", userDetails.getUsername());
-//                model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
-//                return "admin_dashboard";
-//            }
-//        }
-//        return "redirect:/login";
-//    }
-//
-//    @GetMapping("/user/dashboard")
-//    public String userDashboard(@RequestParam String token, Model model) {
-//        if (token != null && token.startsWith("Bearer ")) {
-//            token = token.substring(7);
-//            String username = jwtTokenProvider.extractUsername(token);
-//            UserDetails userDetails = employeeDetailsService.loadUserByUsername(username);
-//            if (jwtTokenProvider.validateToken(token, userDetails)) {
-//                model.addAttribute("token", "Bearer " + token);
-//                model.addAttribute("username", userDetails.getUsername());
-//                model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
-//                return "user_dashboard";
-//            }
-//        }
-//        return "redirect:/login";
-//    }
-    
-    
-    
     @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password, Model model, HttpServletResponse response) {
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
         try {
             Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
             SecurityContextHolder.getContext().setAuthentication(authentication);
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            String token = jwtTokenProvider.generateToken(userDetails);
-            Cookie cookie = new Cookie("token", token);
-            cookie.setHttpOnly(true);
-            cookie.setPath("/");
-            response.addCookie(cookie);
-            model.addAttribute("token",token);
+            String token = "Bearer " + jwtTokenProvider.generateToken(userDetails);
+            model.addAttribute("token", token);
             model.addAttribute("username", userDetails.getUsername());
             model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
             return "home";
@@ -132,37 +56,49 @@ public class PageController {
     }
 
     @GetMapping("/home")
-    public String home(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            model.addAttribute("username", userDetails.getUsername());
-            model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
-            return "home";
+    public String home(@RequestParam String token, Model model) {
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            String username = jwtTokenProvider.extractUsername(token);
+            UserDetails userDetails = employeeDetailsService.loadUserByUsername(username);
+            if (jwtTokenProvider.validateToken(token, userDetails)) {
+                model.addAttribute("token", "Bearer " + token);
+                model.addAttribute("username", userDetails.getUsername());
+                model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
+                return "home";
+            }
         }
         return "redirect:/login";
     }
 
     @GetMapping("/admin/dashboard")
-    public String adminDashboard(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            model.addAttribute("username", userDetails.getUsername());
-            model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
-            return "admin_dashboard";
+    public String adminDashboard(@RequestParam String token, Model model) {
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            String username = jwtTokenProvider.extractUsername(token);
+            UserDetails userDetails = employeeDetailsService.loadUserByUsername(username);
+            if (jwtTokenProvider.validateToken(token, userDetails)) {
+                model.addAttribute("token", "Bearer " + token);
+                model.addAttribute("username", userDetails.getUsername());
+                model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
+                return "admin_dashboard";
+            }
         }
         return "redirect:/login";
     }
 
     @GetMapping("/user/dashboard")
-    public String userDashboard(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            model.addAttribute("username", userDetails.getUsername());
-            model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
-            return "user_dashboard";
+    public String userDashboard(@RequestParam String token, Model model) {
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            String username = jwtTokenProvider.extractUsername(token);
+            UserDetails userDetails = employeeDetailsService.loadUserByUsername(username);
+            if (jwtTokenProvider.validateToken(token, userDetails)) {
+                model.addAttribute("token", "Bearer " + token);
+                model.addAttribute("username", userDetails.getUsername());
+                model.addAttribute("roles", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
+                return "user_dashboard";
+            }
         }
         return "redirect:/login";
     }
